c7b1518514bab28236e67c21ef1c4ca3f6fbbc53
BogdanNecatu <necatubogdan@gmail.com>
2023-05-03 20:04:12 +0200

    first commit

--- main.py
+++ main.py
@@ -10,12 +10,17 @@ def request_api_data(query_char):
         raise RuntimeError(f'Error fetching: {res.status_code}')
     return res
 
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes=(line.split(':') for line in hashes.text)
+    for h, count in hashes:
+        print
+
 def pwned_api_check(password):
-    sha1password = hashlib.sha1(password.encode('utf-8').hexidigit().upper())
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
     first5_char, tail = sha1password[:5], sha1password[5:]
     response = request_api_data(first5_char)
     print(first5_char, tail, response)
-    return response
+    return get_password_leaks_count(response, tail)
 
 
 pwned_api_check('password')
