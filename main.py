684d4ab5a2c5e67cbe58368ab8604a1e610c8104
BogdanNecatu <necatubogdan@gmail.com>
2023-05-03 18:58:56 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# Password check pawen
+import requests
+import hashlib
+
+
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching: {res.status_code}')
+    return res
+
+def pwned_api_check(password):
+    sha1password = hashlib.sha1(password.encode('utf-8').hexidigit().upper())
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    print(first5_char, tail, response)
+    return response
+
+
+# pwned_api_check('123')
